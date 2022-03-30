cd9d1285914c212317c8c3a6234139fb3f5b0d94
Jorian Baeke <jorian@rhinox.be>
2022-03-30 11:24:27 +0200

    odin compatibility

--- Runtime/VHACD.cs
+++ Runtime/VHACD.cs
@@ -2,12 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
+using Unity.Collections;
 using UnityEngine;
+#if ODIN_INSPECTOR
+using Sirenix.Utilities;
+using Sirenix.OdinInspector;
+#endif
+
+#if UNITY_EDITOR && ODIN_INSPECTOR
+using Sirenix.Utilities.Editor;
+using UnityEditor;
+#endif
 
 namespace MeshProcess
 {
     public class VHACD : MonoBehaviour
     {
+        [ShowIf("@_generatedColliders != null && _generatedColliders.Count > 0"), ListDrawerSettings(IsReadOnly = true)]
+        [InlineButton(nameof(ClearColliders), "Clear")]
+        [SerializeField]
+        private List<MeshCollider> _generatedColliders;
+
+        [Title("Parameters"), InlineProperty, HideLabel]
         [System.Serializable]
         public unsafe struct Parameters
         {
@@ -28,61 +44,52 @@ namespace MeshProcess
                 m_convexhullApproximation = 1;
                 m_oclAcceleration = 0;
                 m_maxConvexHulls = 1024;
-                m_projectHullVertices = true; // This will project the output convex hull vertices onto the original source mesh to increase the floating point accuracy of the results
+                m_projectHullVertices =
+                    true; // This will project the output convex hull vertices onto the original source mesh to increase the floating point accuracy of the results
             }
 
-            [Tooltip("maximum concavity")]
-            [Range(0, 1)]
+            [Tooltip("maximum concavity")] [Range(0, 1)]
             public double m_concavity;
 
-            [Tooltip("controls the bias toward clipping along symmetry planes")]
-            [Range(0, 1)]
+            [Tooltip("controls the bias toward clipping along symmetry planes")] [Range(0, 1)]
             public double m_alpha;
 
-            [Tooltip("controls the bias toward clipping along revolution axes")]
-            [Range(0, 1)]
+            [Tooltip("controls the bias toward clipping along revolution axes")] [Range(0, 1)]
             public double m_beta;
 
-            [Tooltip("controls the adaptive sampling of the generated convex-hulls")]
-            [Range(0, 0.01f)]
+            [Tooltip("controls the adaptive sampling of the generated convex-hulls")] [Range(0, 0.01f)]
             public double m_minVolumePerCH;
 
             public void* m_callback;
             public void* m_logger;
 
-            [Tooltip("maximum number of voxels generated during the voxelization stage")]
-            [Range(10000, 64000000)]
+            [Tooltip("maximum number of voxels generated during the voxelization stage")] [Range(10000, 64000000)]
             public uint m_resolution;
 
-            [Tooltip("controls the maximum number of triangles per convex-hull")]
-            [Range(4, 1024)]
+            [Tooltip("controls the maximum number of triangles per convex-hull")] [Range(4, 1024)]
             public uint m_maxNumVerticesPerCH;
 
-            [Tooltip("controls the granularity of the search for the \"best\" clipping plane")]
-            [Range(1, 16)]
+            [Tooltip("controls the granularity of the search for the \"best\" clipping plane")] [Range(1, 16)]
             public uint m_planeDownsampling;
 
             [Tooltip("controls the precision of the convex-hull generation process during the clipping plane selection stage")]
             [Range(1, 16)]
             public uint m_convexhullDownsampling;
 
-            [Tooltip("enable/disable normalizing the mesh before applying the convex decomposition")]
-            [Range(0, 1)]
+            [Tooltip("enable/disable normalizing the mesh before applying the convex decomposition")] [Range(0, 1)]
             public uint m_pca;
 
-            [Tooltip("0: voxel-based (recommended), 1: tetrahedron-based")]
-            [Range(0, 1)]
+            [Tooltip("0: voxel-based (recommended), 1: tetrahedron-based")] [Range(0, 1)]
             public uint m_mode;
 
-            [Range(0, 1)]
-            public uint m_convexhullApproximation;
+            [Range(0, 1)] public uint m_convexhullApproximation;
 
-            [Range(0, 1)]
-            public uint m_oclAcceleration;
+            [Range(0, 1)] public uint m_oclAcceleration;
 
             public uint m_maxConvexHulls;
 
-            [Tooltip("This will project the output convex hull vertices onto the original source mesh to increase the floating point accuracy of the results")]
+            [Tooltip(
+                "This will project the output convex hull vertices onto the original source mesh to increase the floating point accuracy of the results")]
             public bool m_projectHullVertices;
         };
 
@@ -96,9 +103,11 @@ namespace MeshProcess
             public fixed double m_center[3];
         };
 
-        [DllImport("libvhacd")] static extern unsafe void* CreateVHACD();
+        [DllImport("libvhacd")]
+        static extern unsafe void* CreateVHACD();
 
-        [DllImport("libvhacd")] static extern unsafe void DestroyVHACD(void* pVHACD);
+        [DllImport("libvhacd")]
+        static extern unsafe void DestroyVHACD(void* pVHACD);
 
         [DllImport("libvhacd")]
         static extern unsafe bool ComputeFloat(
@@ -118,7 +127,8 @@ namespace MeshProcess
             uint countTriangles,
             Parameters* parameters);
 
-        [DllImport("libvhacd")] static extern unsafe uint GetNConvexHulls(void* pVHACD);
+        [DllImport("libvhacd")]
+        static extern unsafe uint GetNConvexHulls(void* pVHACD);
 
         [DllImport("libvhacd")]
         static extern unsafe void GetConvexHull(
@@ -128,16 +138,15 @@ namespace MeshProcess
 
         public Parameters m_parameters;
 
-        public VHACD() { m_parameters.Init(); }
-
-        [ContextMenu("Generate Convex Meshes")]
-        public unsafe List<Mesh> GenerateConvexMeshes(Mesh mesh = null)
+        public VHACD()
         {
-            if (mesh == null)
-            {
-                mesh = GetComponent<MeshFilter>().sharedMesh;
-            }
-            var vhacd = CreateVHACD();
+            m_parameters.Init();
+        }
+
+		[ContextMenu("Generate Convex Meshes")]
+        private unsafe void GenerateVHACD(Mesh mesh, out void* vhacd, out uint numHulls)
+        {
+            vhacd = CreateVHACD();
             var parameters = m_parameters;
 
             var verts = mesh.vertices;
@@ -147,17 +156,34 @@ namespace MeshProcess
             {
                 ComputeFloat(
                     vhacd,
-                    (float*)pVerts, (uint)verts.Length,
-                    (uint*)pTris, (uint)tris.Length / 3,
+                    (float*) pVerts, (uint) verts.Length,
+                    (uint*) pTris, (uint) tris.Length / 3,
                     &parameters);
             }
 
-            var numHulls = GetNConvexHulls(vhacd);
-            List<Mesh> convexMesh = new List<Mesh>((int)numHulls);
-            foreach (var index in Enumerable.Range(0, (int)numHulls))
+            numHulls = GetNConvexHulls(vhacd);
+        }
+
+        private void ClearColliders()
+        {
+            if (!_generatedColliders.IsNullOrEmpty())
+            {
+                foreach (var collider in _generatedColliders)
+                    DestroyImmediate(collider);
+                _generatedColliders.Clear();
+            }
+
+            if (_generatedColliders == null)
+                _generatedColliders = new List<MeshCollider>();
+        }
+
+        public unsafe List<Mesh> GenerateConvexMeshes(void* vhacd, int numHulls)
+        {
+            List<Mesh> convexMesh = new List<Mesh>(numHulls);
+            for (uint index = 0; index < numHulls; ++index)
             {
                 ConvexHull hull;
-                GetConvexHull(vhacd, (uint)index, &hull);
+                GetConvexHull(vhacd, index, &hull);
 
                 var hullMesh = new Mesh();
                 var hullVerts = new Vector3[hull.m_nPoints];
@@ -168,9 +194,9 @@ namespace MeshProcess
 
                     for (var pointCount = hull.m_nPoints; pointCount != 0; --pointCount)
                     {
-                        pVerts->x = (float)pComponents[0];
-                        pVerts->y = (float)pComponents[1];
-                        pVerts->z = (float)pComponents[2];
+                        pVerts->x = (float) pComponents[0];
+                        pVerts->y = (float) pComponents[1];
+                        pVerts->z = (float) pComponents[2];
 
                         pVerts += 1;
                         pComponents += 3;
@@ -180,13 +206,98 @@ namespace MeshProcess
                 hullMesh.SetVertices(hullVerts);
 
                 var indices = new int[hull.m_nTriangles * 3];
-                Marshal.Copy((System.IntPtr)hull.m_triangles, indices, 0, indices.Length);
+                Marshal.Copy((System.IntPtr) hull.m_triangles, indices, 0, indices.Length);
                 hullMesh.SetTriangles(indices, 0);
 
 
                 convexMesh.Add(hullMesh);
             }
+
             return convexMesh;
         }
+
+#if UNITY_EDITOR && ODIN_INSPECTOR
+
+        private const string GEN_NAME = "[Generated] VHACD";
+        private unsafe struct VHACD_Info
+        {
+            public void* VHACD;
+            public int NumHulls;
+            public MeshFilter MeshFilter;
+
+            public Transform GetOrCreateRoot()
+            {
+                var root = MeshFilter.transform.Find(GEN_NAME);
+                if (root == null)
+                {
+                    root = new GameObject(GEN_NAME).transform;
+                    root.SetParent(MeshFilter.transform, false);
+                }
+
+                return root;
+            }
+
+        }
+
+        [OnInspectorGUI, PropertyOrder(-1)]
+        private unsafe void OnInspectorGui()
+        {
+            var meshFilters = GetComponentsInChildren<MeshFilter>(true);
+            if (!meshFilters.Any())
+            {
+                SirenixEditorGUI.InfoMessageBox("You need a meshfilter(s) to generate a v-vacd collider.");
+                return;
+            }
+
+            if (GUILayout.Button("Generate Collider"))
+            {
+                List<VHACD_Info> infos = new List<VHACD_Info>();
+                foreach (var meshFilter in meshFilters)
+                {
+                    var mesh = meshFilter.sharedMesh;
+                    if (mesh)
+                    {
+                        GenerateVHACD(meshFilter.sharedMesh, out var vhacd, out var numHulls);
+                        infos.Add(new VHACD_Info
+                        {
+                            MeshFilter = meshFilter,
+                            NumHulls = (int) numHulls,
+                            VHACD = vhacd
+                        });
+                    }
+                }
+
+                var totalMeshes = infos.Sum(x => x.NumHulls);
+
+                if (totalMeshes > 10)
+                {
+                    var cont = EditorUtility.DisplayDialog(
+                        "Lots of meshes",
+                        $"A total of {totalMeshes} colliders will be generated. Are you sure you wish to continue?",
+                        "Yes", "No");
+
+                    if (!cont) return;
+                }
+
+                ClearColliders();
+
+                foreach (var info in infos)
+                {
+                    var root = info.GetOrCreateRoot();
+                    var meshes = GenerateConvexMeshes(info.VHACD, info.NumHulls);
+
+                    foreach (var mesh in meshes)
+                    {
+                        var collider = root.gameObject.AddComponent<MeshCollider>();
+                        collider.sharedMesh = mesh;
+                        collider.convex = true;
+
+                        _generatedColliders.Add(collider);
+                    }
+                }
+
+            }
+        }
+#endif
     }
 }
